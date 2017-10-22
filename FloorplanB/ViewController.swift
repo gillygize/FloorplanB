e052acfe0863031591244fa153f64a921f01f765
Matthew Gillingham <me@mattgillingham.com>
2017-10-22 19:41:14 +0900

    Update example view

--- FloorplanB/ViewController.swift
+++ FloorplanB/ViewController.swift
@@ -27,10 +27,25 @@ class ViewController: UIViewController {
     view.addSubview(firstView)
     view.addSubview(secondView)
 
+    layout()
+  }
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    layout()
+  }
+
+  override func viewSafeAreaInsetsDidChange() {
+    super.viewSafeAreaInsetsDidChange()
+    layout()
+  }
+
+  func layout() {
     [
       firstView,
       secondView
-    ].resize(withSize: CGSize(width: 100, height: 100))
+    ].resize(withSize: CGSize(width: view.bounds.size.width - 20, height: 100))
+     .reposition(withTopLeftCorner: CGPoint(x: view.safeAreaInsets.left + 10, y: view.safeAreaInsets.top))
      .space(withHeight: 10)
      .arrangeTopToBottom(fromTop: 0)
      .toArray()
